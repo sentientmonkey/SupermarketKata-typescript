3e2e94d7cf03324224a093b2a17326e0d71a2b97
Scott Windsor <swindsor@gmail.com>
2020-06-19 14:58:58 -0700

    Discount Pricing

--- spec/supermarket.spec.ts
+++ spec/supermarket.spec.ts
@@ -1,20 +1,33 @@
 import "jasmine";
-import { Item, ShoppingCart, Stock } from "../src/supermarket";
+import { Item, DiscountItem, ShoppingCart, Stock } from "../src/supermarket";
 
 describe("Shopping Cart", () => {
   it("should calculate total for items", () => {
     const subject = buildCart(
-      new Item("BEAN", "beans", 65),
+      new Item("BEANS", "beans", 65),
       new Item("SAUS", "sausage", 125),
     );
 
+    subject.scan("BEANS");
+    subject.scan("SAUS");
+
     expect(subject.total()).toBe(190);
+  });
+
+  it("should handle special pricing", () => {
+    const subject = buildCart(new DiscountItem("APP", "apples", 30, 25, 3));
+
+    subject.scan("APP");
+    subject.scan("APP");
+    expect(subject.total()).toBe(60);
+
+    subject.scan("APP");
+    expect(subject.total()).toBe(75);
   });
 
   function buildCart(...items: Item[]): ShoppingCart {
     const stock = new Stock(items);
     const cart = new ShoppingCart(stock);
-    items.forEach(item => cart.scan(item.sku));
     return cart;
   }
 });
--- src/supermarket.ts
+++ src/supermarket.ts
@@ -1,12 +1,43 @@
 export class Item {
   sku: string;
   name: string;
-  price: number;
+  itemPrice: number;
+  hasDiscount: boolean = false;
 
-  constructor(sku: string, name: string, price: number) {
+  constructor(sku: string, name: string, itemPrice: number) {
     this.sku = sku;
     this.name = name;
-    this.price = price;
+    this.itemPrice = itemPrice;
+  }
+
+  price(quantity: number): number {
+    return this.itemPrice * quantity;
+  }
+}
+
+export class DiscountItem extends Item {
+  discountPrice: number;
+  discountQuantity: number;
+  hasDiscount: boolean = true;
+
+  constructor(
+    sku: string,
+    name: string,
+    itemPrice: number,
+    discountPrice: number,
+    discountQuantity: number,
+  ) {
+    super(sku, name, itemPrice);
+    this.discountPrice = discountPrice;
+    this.discountQuantity = discountQuantity;
+  }
+
+  price(quantity: number): number {
+    const discounted =
+      Math.floor(quantity / this.discountQuantity) * this.discountQuantity;
+    const regular = quantity % this.discountQuantity;
+
+    return this.itemPrice * regular + this.discountPrice * discounted;
   }
 }
 
@@ -21,22 +52,42 @@ export class Stock {
   }
 }
 
+export class CartItem {
+  item: Item;
+  quantity: number = 1;
+
+  constructor(item: Item) {
+    this.item = item;
+  }
+
+  price(): number {
+    return this.item.price(this.quantity);
+  }
+}
+
 export class ShoppingCart {
   stock: Stock;
-  items: Item[] = [];
+  items: CartItem[] = [];
 
   constructor(stock: Stock) {
     this.stock = stock;
   }
 
   scan(sku: string) {
-    const item = this.stock.lookup(sku);
-    if (item) {
-      this.items.push(item);
+    let cartItem = this.items.find(cartItem => cartItem.item.sku === sku);
+    if (!cartItem) {
+      const item = this.stock.lookup(sku);
+      if (!item) {
+        throw new Error(`Could not find ${sku}`);
+      }
+      cartItem = new CartItem(item);
+      this.items.push(cartItem);
+    } else {
+      cartItem.quantity++;
     }
   }
 
   total(): number {
-    return this.items.reduce((a, b) => a + b.price, 0);
+    return this.items.reduce((a, b) => a + b.price(), 0);
   }
 }
